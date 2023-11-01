bc0663e2daecf7301b3bb72bc0038a12e86007be
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-11-01 11:05:40 +0100

    update exports

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ export * from "./documents/index.js";
 export * from "./generators/index.js";
 export * from "./utils/index.js";
 
-export * as draft04 from "./documents/draft-04/index.js";
-export * as draft202012 from "./documents/draft-2020-12/index.js";
-export * as intermediateB from "./documents/intermediate-b/index.js";
+export * as schemaDraft04 from "./documents/draft-04/index.js";
+export * as schemaDraft202012 from "./documents/draft-2020-12/index.js";
+export * as schemaIntermediateB from "./documents/intermediate-b/index.js";

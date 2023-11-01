10f14af195fa1747b618ca77ecd7c92f1df04869
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-11-01 11:00:17 +0100

    export documents, fix prepare bug (#48)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
 export * from "./documents/index.js";
 export * from "./generators/index.js";
 export * from "./utils/index.js";
+
+export * as draft04 from "./documents/draft-04/index.js";
+export * as draft202012 from "./documents/draft-2020-12/index.js";
+export * as intermediateB from "./documents/intermediate-b/index.js";

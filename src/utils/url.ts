02ca16ef08bf975474a35ff2457ae60a59125639
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-11-02 17:42:16 +0100

    fix normalize

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,12 +3,12 @@ export function normalizeUrl(url: URL) {
     return new URL("#", url);
   }
 
-  url.hash = url.hash.replaceAll(/%([0-9A-F]{2})/g, ($0, $1) => {
+  url.hash = url.hash.replaceAll(/%([0-9A-F]{2})/gi, ($0, $1) => {
     const value = parseInt($1, 16);
     const char = String.fromCharCode(value);
     switch (char) {
       case "/":
-        return $0;
+        return $0.toUpperCase();
       default:
         return char;
     }
